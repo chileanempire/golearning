b11702b8ab10ad38602fd35e01866efd341f30fc
Tomasz Siepsiak <tomasz.siepsiak@gmail.com>
2014-12-07 16:08:23 +0100

    third working version

--- cmd/helloworld/main.go
+++ cmd/helloworld/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/chileanempire/golearning/helloutils"
 )
 
 func main() {
+	count := 5
+	if len(os.Args) == 2 {
+		if n, err := strconv.Atoi(os.Args[1]); err == nil {
+			count = n
+		}
+	}
 	fmt.Println("Hello, world!")
 	fmt.Println("Current time is", time.Now())
-	if err := helloutils.XD(11); err != nil {
+	if err := helloutils.XD(count); err != nil {
 		panic(err)
 	}
 }
